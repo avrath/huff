e9b339b697bb76899ee3ae3713ea9d795b396d5e
paul105 <graficiak@gmail.com>
2014-04-21 21:38:25 +0200

    dodanie komentarzy

--- entropia.m
+++ entropia.m
@@ -1,19 +1,15 @@
 function [ hh,efekt ] = entropia(kod,l_znak,znaki,slownik )
-%ENTROPIA Summary of this function goes here
-%   Detailed explanation goes here
-%tekst='ala ma kota';
-%[kod,slownik,l_znak,znaki]=HUFF2(tekst);
-l_znak;
-znaki;
-slownik;
+%wyliczenie wektora prawdopodobienstwa wystepowania znaku
 probab=((cell2mat(slownik(:,3)))./sum(cell2mat(slownik(:,3))))';
 
+%obliczenie entropii oraz dlugosci kodu
 for i=1:length(l_znak)
     L(i)=probab(i)*length(slownik{i,2});
     H(i)=-(probab(i)*log2(probab(i)));
 end
 ll=sum(L);
 hh=sum(H);
+%wyznaczenie efektywnosci ciagu kodowego
 efekt=(hh*100)/ll;
 
 end

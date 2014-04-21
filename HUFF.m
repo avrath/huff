548ce653c13f707e90023196a378c7b47b3e6e63
avrath <avrath@gmail.com>
2014-04-21 18:20:13 +0200

    kolejny update

--- HUFF.m
+++ HUFF.m
@@ -5,7 +5,10 @@ global ZAKODOWANE_ZNAKI
 global ii
 ii=0;
 
-bez_powt=unique(tekst); 
+
+bez_powt=unique(tekst);  
+
+if (length(bez_powt)>1) 
     
 for i=1:length(bez_powt)    
     ilosc_znakow(i) = length(find(tekst == bez_powt(i))); 
@@ -17,27 +20,46 @@ for i=1:length(index)
     posortowane_znaki{i}=znaki{index(i)};
 end
 
-%ilosc_znakow
-%posortowane_znaki
+
+
 
 drzewo = cell(length(ilosc_znakow), 1);
 
 
-for i = 1:length(ilosc_znakow) 
+for i = 1:length(ilosc_znakow)
    drzewo{i} = i;              
 end
 
 
 while length(ilosc_znakow) > 2
-   [ilosc_znakow, index] = sort(ilosc_znakow);   
-   ilosc_znakow(2) = ilosc_znakow(1) + ilosc_znakow(2); 
+   [ilosc_znakow, index] = sort(ilosc_znakow);
+   ilosc_znakow(2) = ilosc_znakow(1) + ilosc_znakow(2);  
    ilosc_znakow(1) = [];           
    
-   drzewo = drzewo(index);           
+   drzewo = drzewo(index);            
    drzewo{2} = {drzewo{1}, drzewo{2}}; 
    drzewo(1) = [];           
 end
 
-kodowanie(drzewo, [])
+kodowanie(drzewo, []) 
 
-%ZAKODOWANE_ZNAKI
+for i=1:length(tekst)               
+    for k=1:length(posortowane_znaki)
+        if posortowane_znaki{k} == tekst(i)
+            kod{i} = ZAKODOWANE_ZNAKI{k};
+            break
+        end
+    end
+end
+
+else 
+    for i=1:length(tekst) 
+    posortowane_znaki{i}=bez_powt;
+    ZAKODOWANE_ZNAKI{i}=('1');  
+    %kod{i}=ZAKODOWANE_ZNAKI{i};
+    end
+    
+    kod=ZAKODOWANE_ZNAKI
+end
+
+zdekodowany=dekodowanie(kod, posortowane_znaki);

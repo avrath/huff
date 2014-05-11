d344ac0469515f10e3c5b1924b9e8c354166d50a
avrath <avrath@gmail.com>
2014-05-11 21:56:35 +0200

    zakomentowanie

--- kodowanie.m
+++ kodowanie.m
@@ -1,12 +1,15 @@
 function kodowanie(drzewo, znak)
 
+%uzycie zmiennych globalnych, poniewaz potrzeba zeby byly znane
+%poszczegolnym rekurencyjnym wywolaniom funkcji
+
 global ZAKODOWANE_ZNAKI;
 global ii;
 
-if isa(drzewo, 'cell')   
-   kodowanie(drzewo{1}, [znak 0])    
-   kodowanie(drzewo{2}, [znak 1])    
+if isa(drzewo, 'cell')   %sprawdzenie czy komorka, czy konkretna wartosc
+   kodowanie(drzewo{1}, [znak 0])    % rekurencja, wbijanie sie 'glebiej' az nie bedzie juz
+   kodowanie(drzewo{2}, [znak 1])    % komorek i przypisywanie '0' lub '1'
 else
    ii=ii+1;
-   ZAKODOWANE_ZNAKI{ii} = char('0'+znak);  
+   ZAKODOWANE_ZNAKI{ii} = char('0'+znak);  %jesli wartosc, to tylko przypisanie '0' lub '1'
 end

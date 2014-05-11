64a58ca0b65173a0ab3e471e0c5699f147ca80d1
avrath <avrath@gmail.com>
2014-05-11 21:57:04 +0200

    zakomentowanie

--- dekodowanie.m
+++ dekodowanie.m
@@ -1,7 +1,7 @@
 function [zdekodowany] = dekodowanie(kod, posortowane_znaki)
 global ZAKODOWANE_ZNAKI;
 
-kod_string=cat(2, kod{:});            
+kod_string=cat(2, kod{:});   %string zawierajacy zakodowany tekst
 kk=1;
 iii=1;
 for i=1:length(kod_string)
@@ -9,16 +9,13 @@ for i=1:length(kod_string)
         kk=i;
     end
     for k=1:length(ZAKODOWANE_ZNAKI)
-        if strcmpi(kod_string(kk:i),ZAKODOWANE_ZNAKI{k}) 
-            zdekodowany(iii)=posortowane_znaki{k};
+        if strcmpi(kod_string(kk:i),ZAKODOWANE_ZNAKI{k})    %sprawdzenie czy dany kawalek kodu pokrywa
+            zdekodowany(iii)=posortowane_znaki{k};          %sie z ktoryms z slow kodowych
             iii=iii+1;
             i=i+length(ZAKODOWANE_ZNAKI{k})-1;
             kk=i+1;
             break
         end
-            
-            
-   
     end
 end
 end
